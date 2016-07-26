1c1a9e22aa2a505b23d17eee537c5cc9ce57efbf
Mac <Mac@Macs-iMac.local>
2016-07-26 10:33:53 -0500

    further organized template files

--- CollegeBot/urls.py
+++ CollegeBot/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     #url(r'^custom$', views.custom, name='custom'),
     url(r'^(?P<college_id>\d+)/$', 'CollegeBot.views.college'),
     url(r'^api/(?P<pk>\d+)/$', views.CollegeDetail.as_view()),
+    url(r'^coefficient/(?P<college_id>\d+)/$', 'CollegeBot.views.coefficient'),
+    url(r'^api/(?P<pk>\d+)/$', views.CoefficientDetail.as_view()),
+    url(r'^custom_coefficient/(?P<college_id>\d+)/$', 'CollegeBot.views.custom_coefficient'),
+    url(r'^api/(?P<pk>\d+)/$', views.Custom_CoefficientDetail.as_view()),
 ]
--- CollegeBot/views.py
+++ CollegeBot/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 #from django.utils import simplejson
 from django.http import HttpResponse, HttpResponseRedirect
-from .models import College, Coefficient, Custom_Coefficient
+from .models import Student, College, Coefficient, Custom_Coefficient
 #from django.shortcuts import render_template_to_response
 from rest_framework import generics
-from .serializers import CollegeSerializer
+from .serializers import CollegeSerializer, CoefficientSerializer, Custom_CoefficientSerializer
 # created by Yazan Kittaneh 07-12-2016
 # *1 cite: https://stackoverflow.com/questions/14160221/django-read-upload-a-csv
 
@@ -56,6 +56,21 @@ def college(request, college_id):
 """
 COEFFICIENT MODEL CONTROLLERS
 """
+def all_custom_coefficients(request):
+    all_custom_coefficients = Custom_Coefficient.objects.all()
+    return render(request, 'CollegeBot/all_custom_coefficients.html', {'all_custom_coefficients': all_custom_coefficients})
+
+class Custom_CoefficientDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Custom_Coefficient.objects.all()
+    serializer_class = Custom_CoefficientSerializer
+
+def custom_coefficient(request, coefficient_id):
+    custom_coefficient = Custom_Coefficient.objects.get(pk=coefficient_id)
+    return render(request, 'CollegeBot/Custom_Coefficient.html', {'Custom_Coefficient':coefficient})
+
+"""
+CUSTOM COEFFICIENT MODEL CONTROLLERS
+"""
 def all_coefficients(request):
     all_coefficients = Coefficient.objects.all()
     return render(request, 'CollegeBot/all_coefficients.html', {'all_coefficients': all_coefficients})
@@ -67,18 +82,3 @@ class CoefficientDetail(generics.RetrieveUpdateDestroyAPIView):
 def coefficient(request, coefficient_id):
     coefficient = Coefficient.objects.get(pk=coefficient_id)
     return render(request, 'CollegeBot/Coefficient.html', {'Coefficient':coefficient})
-
-"""
-CUSTOM COEFFICIENT MODEL CONTROLLERS
-"""
-def all_coefficients(request):
-    all_coefficients = Coefficient.objects.all()
-    return render(request, 'CoefficientBot/all_coefficients.html', {'all_coefficients': all_coefficients})
-
-class CoefficientDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Coefficient.objects.all()
-    serializer_class = CoefficientSerializer
-
-def coefficient(request, coefficient_id):
-    coefficient = Coefficient.objects.get(pk=coefficient_id)
-    return render(request, 'CoefficientBot/Coefficient.html', {'Coefficient':coefficient})
