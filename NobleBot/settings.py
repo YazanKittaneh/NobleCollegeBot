0d0c3fa03cb56f030af13d39fc317308c735507b
yazankittaneh <yazanakittaneh@gmail.com>
2016-08-22 09:04:03 -0500

    manage.py changes

--- NobleBot/settings.py
+++ NobleBot/settings.py
@@ -24,10 +24,10 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%n&ztfblhecd90_92yn=*q%wd-pv3qo8fiz3bw5xxx@etgu(b+'
+#SECRET_KEY = '%n&ztfblhecd90_92yn=*q%wd-pv3qo8fiz3bw5xxx@etgu(b+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = False
 
 ALLOWED_HOSTS = []
 
